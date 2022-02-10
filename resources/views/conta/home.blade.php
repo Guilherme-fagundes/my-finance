51bdb9f493a1143320ff802cbcac323c24c6bc59
Guilherme-fagundes <guilhermekfagundes259@gmail.com>
2022-02-10 03:41:06 -0300

    Concluindo card das estatisticas

--- resources/views/conta/home.blade.php
+++ resources/views/conta/home.blade.php
@@ -8,14 +8,26 @@
         <div class="container">
             <div class="row py-2 statisticas">
 
-                <div class="despesas py-4 rounded-3 ">
-                    Despesas
+                <div class="despesas pt-3 rounded-3 ">
+                    <p class="title">Despesas</p>
+                    <p class="valor">
+                        <span class="icon">R$</span>
+                        <span class="text">950,00</span>
+                    </p>
                 </div>
-                <div class="receitas py-4 rounded-3" style="background-color: #198754">
-                    Receitas
+                <div class="receitas pt-3 rounded-3" style="background-color: #198754">
+                    <p class="title">Receitas</p>
+                    <p class="valor">
+                        <span class="icon">R$</span>
+                        <span class="text">3.520,00</span>
+                    </p>
                 </div>
-                <div class="saldo py-4 bg-primary rounded-3">
-                    Saldo
+                <div class="saldo pt-3 bg-primary rounded-3">
+                    <p class="title">Saldo</p>
+                    <p class="valor">
+                        <span class="icon">R$</span>
+                        <span class="text">20.880,00</span>
+                    </p>
                 </div>
 
             </div>
