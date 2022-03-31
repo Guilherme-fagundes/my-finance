286be6022e852c615cdc47e162d1c829f029c19a
Guilherme-fagundes <guilhermekfagundes259@gmail.com>
2022-03-31 01:42:15 -0300

    Iniciando implementação de modal e formulario de nova carteira

--- resources/views/conta/carteiras/listar.blade.php
+++ resources/views/conta/carteiras/listar.blade.php
@@ -33,7 +33,7 @@
                         <div class="card-body">
                             <h2 class="card-title text-center"><i class="fa-solid fa-wallet"></i></h2>
                             <p class="card-text my-5 text-center">Para criar uma nova carteira clique aqui.</p>
-                            <p class="text-center addNewWallet"><span><i class="fa-solid fa-circle-plus"></i> Adicionar nova carteira</span></p>
+                            <p class="text-center addNewWallet" data-toggle="modal" data-target="#criarNovaCarteira"><span><i class="fa-solid fa-circle-plus"></i> Adicionar nova carteira</span></p>
                         </div>
                     </div>
 
@@ -43,7 +43,37 @@
 
             </div>
 
-        </div>
+
+
+            <!-- Modal criar nova carteira -->
+            <div class="modal fade" id="criarNovaCarteira" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title" id="exampleModalLongTitle">Criar nova carteira</h1>
+
+                        </div>
+                        <div class="modal-body">
+                            <form>
+                                <div class="mb-3">
+                                    <label class="form-label"><i class="fa-solid fa-wallet"></i> Descrição</label>
+                                    <input class="form-control" type="text" name="descricao" placeholder="Descrição ou nome da carteira">
+
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label"><i class="fa-solid fa-paintbrush"></i> Selecione a cor para a carteira</label><br>
+                                    <input class="form-control-color" value="#0078FF" type="color" name="cor_carteira">
+
+                                </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
     </section>
 @endsection
