cf32672f724912440aef719f498d686bfbcdc850
Guilherme-fagundes <guilhermekfagundes259@gmail.com>
2022-04-01 07:20:54 -0300

    Adicionando listagem das carteiras

--- resources/views/conta/carteiras/listar.blade.php
+++ resources/views/conta/carteiras/listar.blade.php
@@ -39,6 +39,26 @@
 
                 </div>
 
+                <div class="col-12 col-md-4 mb-4">
+
+                    <div class="card cardWallet w-100" data-carteira-id="">
+                        <div class="card-body">
+                            <h2 class="card-title mb-5 text-center"><i class="fa-solid fa-wallet"></i> Teste</h2>
+                            <p class="card-text text-center"><span>Despesas: R$ 2.500,00</span></p>
+                            <p class="card-text my-2 text-center"><span>Receitas: R$ 10.000,00</span></p>
+                            <div class="walletActions">
+                                <p>
+                                    <a href="#" title="Abrir carteira" class="walletView"><i class="fa-solid fa-eye"></i></a>
+                                    <a href="#" title="Editar carteira" class="walletEdit"><i class="fa-solid fa-pen"></i></a>
+                                    <a href="#" title="Excluir carteira" class="walletTrash"><i class="fa-solid fa-circle-xmark"></i></a>
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
 
 
             </div>
