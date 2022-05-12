f510be4771d821b7da88085534b281633bf15a46
Guilherme-fagundes <guilhermekfagundes259@gmail.com>
2022-05-11 21:00:32 -0300

    Responsivando tabela de relatorios de lançamentos

--- resources/views/conta/carteiras/abrir.blade.php
+++ resources/views/conta/carteiras/abrir.blade.php
@@ -20,16 +20,21 @@
 
     <section class="sessWallet">
         <div class="container">
-            <div class="row py-2 rowTitleWallet d-flex justify-content-between align-items-center flex-wrap">
+            <div class="row py-2 rowTitleWallet">
                 <div class="col-12 col-md-6">
                     <h1 class="tituloMinhaCarteira mb-0"><i class="fa-solid fa-wallet"></i>
                         Carteira {{ strtolower($wallet->nome) }}</h1>
                 </div>
 
                 <div class="col-12 col-md-6 walletHeader">
-                    <a href="{{ route('carteiras.listar') }}" class="btn btn-outline-secondary btn-sm btn-voltar"><i class="fa-solid fa-arrow-left"></i> Voltar</a>
-                    <a href="#" class="btn btn-danger btn-sm btn-nova-despesa"><i class="fa-solid fa-circle-plus"></i>
-                        Lançar nova despesa</a>
+                    <a href="{{ route('carteiras.listar') }}" class="btn btn-outline-secondary btn-sm btn-voltar">
+                        <span class="icon"><i class="fa-solid fa-arrow-left"></i></span>
+                        <span class="text">Voltar</span>
+                    </a>
+                    <a href="#" class="btn btn-danger btn-sm btn-nova-despesa">
+                        <span class="icon"><i class="fa-solid fa-circle-plus"></i></span>
+                        <span class="text">Lançar nova despesa</span>
+                    </a>
                     <a href="#" class="btn btn-success btn-sm btn-nova-renda">
                         <span class="icon"><i class="fa-solid fa-circle-plus"></i></span>
                         <span class="text"> Lançar nova renda</span>
