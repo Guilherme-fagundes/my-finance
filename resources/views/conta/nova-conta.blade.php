be7d328494d513f1e98c7aadbfa52b15b7c1ae24
Guilherme-fagundes <guilhermekfagundes259@gmail.com>
2022-05-24 22:56:12 -0300

    Removendo comentarios

--- resources/views/conta/nova-conta.blade.php
+++ resources/views/conta/nova-conta.blade.php
@@ -78,11 +78,7 @@
 </div>
 
 <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-<script type="text/javascript">
-    // var onloadCallback = function() {
-    //     alert("grecaptcha is ready!");
-    // };
-</script>
+
 <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
         async defer>
 </script>
