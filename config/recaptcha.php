37fa1192e0bba2d08d4c3b51a63a54ba64147bd8
Guilherme-fagundes <guilhermekfagundes259@gmail.com>
2022-05-26 09:45:18 -0300

    corrigindo workflow

--- config/recaptcha.php
+++ config/recaptcha.php
@@ -4,7 +4,7 @@
  * Copyright (c) 2017 - present
  * LaravelGoogleRecaptcha - recaptcha.php
  * author: Roberto Belotti - roby.belotti@gmail.com
- * web : robertobelotti.com, github.com/biscolab
+ * web : robertobelotti.com, .github.com/biscolab
  * Initial version created on: 12/9/2018
  * MIT license: https://github.com/biscolab/laravel-recaptcha/blob/master/LICENSE
  */
